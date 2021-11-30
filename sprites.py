2329ddd4d0afa8505e2830c48920ed38114f9e37
Lucas Tachdjian <lucasct1@al.insper.edu.br>
2021-11-30 12:01:31 -0300

    Amem a imagem foi

--- sprites.py
+++ sprites.py
@@ -1,19 +1,49 @@
 # Sprite classes for platform game
 import pygame as pg
 from settings import *
+from os import path
 vec = pg.math.Vector2
+
+
+
+
 
 class Player(pg.sprite.Sprite):
     def __init__(self):
         #personagem
+        img_dir = path.join(path.dirname(__file__),'img')
+        # Carregando as imagens
+        froggy1 = pg.image.load(path.join(img_dir,"Froggy1.png")).convert()
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((30, 40))
-        self.image.fill(VERDE)
+        self.image = froggy1
+
         self.rect = self.image.get_rect()
         self.rect.center = (LARGURA / 2, ALTURA / 2)
         self.pos = vec(LARGURA / 2, ALTURA / 2)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
+
+    def import_image(self):
+
+        self.animacao = {'puland':[] , 'caindo':[]}
+
+
+    def animacao(self):
+        animacao = self.animation[self.status]
+
+        #loop para animar
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animacao):
+            self.frame_index = 0
+            # 'desbuga' o loop, sempre retoma ao inicio apos acabar as imagens
+        self.image = animacao[int(self.frame_index)]
+    
+    def status(self):
+        if self.vel.y < 0 :
+            self.status = 'pulando'
+        if self.vel.y > 0:
+            self.status = 'caindo'
+
 
     def update(self):
         #movimentacao com setas do teclado
